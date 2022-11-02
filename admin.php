80730997a6ae201eebae121ba875c49b652a15e2
Thibaut <tbourlier@normandiewebschool.fr>
2022-11-02 22:28:57 +0100

    READ + objet student

--- admin.php
+++ admin.php
@@ -1,6 +1,19 @@
-<title>Interface Administrateur</title>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="image/logo_nws_item.svg"  rel="shortcut icon">
+    <link href="style.css" rel="stylesheet">
+    <title>Interface Administrateur</title>
+</head>
+<body>
+    
+</body>
+</html>
 
-<h1 class=h1>Gestions des étudiants</h1>
+<h1 class=titre>Gestions des étudiants</h1>
 <table>
     <tr>
         <td scope="row"><strong>ID</strong></td>
@@ -18,3 +31,63 @@
         <td width = 7%><strong>Sexe</strong></td>
         <td width = 7%><strong>Statut</strong></td>
     </tr>
+
+    
+
+<?php
+
+require_once('database.php');
+require_once('function.php');
+
+try {
+    foreach ($newdb->query('SELECT * FROM `student`') as $row) {
+        $row = array_map("utf8_encode", $row);
+
+        $id = $row ['student_id'];
+        $firstName = $row['student_fname'];
+        $lastName = $row['student_lname'];
+        $mail = $row['student_mail'];
+        $dateOfBirth = $row['student_dateofbirth'];
+        $phoneNumber = $row['student_phonenumber'];
+        $street = $row['address_street'];
+        $country = $row['address_country'];
+        $postalCode = $row['address_postal_code'];
+        $city = $row['address_city'];
+        $year = $row['student_annee'];
+        $speciality = $row['student_speciality'];
+        $gender = $row['student_gender'];
+        $status = $row['student_status'];
+
+
+        print "<tr> 
+        
+        <td>$id</td>
+        <td>$firstName</td>
+        <td>$lastName</td>
+        <td>$mail</td>
+        <td>$dateOfBirth</td>
+        <td>$phoneNumber</td>
+        <td>$street</td>
+        <td>$city</td>
+        <td>$postalCode</td>
+        <td>$country</td>
+        <td>$year</td>
+        <td>$speciality</td>
+        <td>$gender</td>
+        <td>$status</td>
+
+        ";
+    }
+}
+catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+    die;
+}
+
+?>
+
+    <div class="nav">
+        <button class="bouton_admin">
+            <a href="index.html">Interface utilisateur</a>
+        </button>
+    </div>
